2b61b94587bb47fa7724a0714291049fee094cb8
carlodipasupil <2015081575@ust-ics.mygbiz.com>
2018-11-30 20:56:06 +0800

    Added Firebase database for shopping list

--- app/src/main/java/yummers/com/ShoppingList.java
+++ app/src/main/java/yummers/com/ShoppingList.java
@@ -2,27 +2,40 @@ package yummers.com;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.ListView;
+import android.widget.SearchView;
 import android.widget.TextView;
+
+import com.google.firebase.database.ChildEventListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class ShoppingList extends AppCompatActivity {
 
-    EditText et;
-    Button bt;
-    ListView lv;
-    ArrayList<String> arrayList;
+    EditText input;
+    Button addBtn;
+    ListView mListView;
+    ArrayList<String> mShoppingList = new ArrayList<>();
+
     ArrayAdapter<String> adapter;
 
-    String shoppingList[];
+    private DatabaseReference mDatabase;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,14 +44,46 @@ public class ShoppingList extends AppCompatActivity {
         Intent i = new Intent(this, YummersService.class);
         Bundle extras = getIntent().getExtras();
 
-        et = (EditText) findViewById(R.id.editText);
-        bt = (Button) findViewById(R.id.addToList);
-        lv = (ListView) findViewById(R.id.shoppingList);
+        input = (EditText) findViewById(R.id.food_field);
+        addBtn = (Button) findViewById(R.id.addToList);
 
-        arrayList = new ArrayList<String>();
-        adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, arrayList);
+        mDatabase = FirebaseDatabase.getInstance().getReference().child("Shopping_List");
+        mListView = (ListView) findViewById(R.id.shoppingList);
 
-        lv.setAdapter(adapter);
+        adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, mShoppingList);
+
+        mListView.setAdapter(adapter);
+
+
+        mDatabase.addChildEventListener(new ChildEventListener() {
+            @Override
+            public void onChildAdded(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
+                String value = dataSnapshot.getValue(String.class);
+                mShoppingList.add(value);
+                adapter.notifyDataSetChanged();
+            }
+
+            @Override
+            public void onChildChanged(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
+
+            }
+
+            @Override
+            public void onChildRemoved(@NonNull DataSnapshot dataSnapshot) {
+
+            }
+
+            @Override
+            public void onChildMoved(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+
 
         onBtnClick();
 
@@ -47,15 +92,19 @@ public class ShoppingList extends AppCompatActivity {
     }
 
     public void onBtnClick(){
-        bt.setOnClickListener(new View.OnClickListener(){
+        addBtn.setOnClickListener(new View.OnClickListener(){
 
             @Override
             public void onClick(View v){
-               String result =  et.getText().toString();
-                arrayList.add(result);
-                adapter.notifyDataSetChanged();
+                String value =  input.getText().toString();
+                mDatabase.push().setValue(value);
+
             }
         });
 
     }
+
+
+
+
 }
